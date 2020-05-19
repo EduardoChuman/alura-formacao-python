dcb91b4da10b32824504e01d5a304f2ddc8dfc21
EDUARDO CHIAKI CHUMAN <eduardo.chuman@caixa.gov.br>
2020-05-19 00:40:05 -0300

    estudando o segundo curso da trilha de python da Alura

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,7 +4,40 @@ def jogar():
     print("{:*^60}" .format("*"))
     print(" ")
 
-    print("Jogando forca...")
+    palavra_secreta = "banana"
+    lista_palavra_secreta = ["_" for posicao in palavra_secreta]
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while not enforcou and not acertou:
+        print(" ")
+        print(f"Palavra secreta : {lista_palavra_secreta}")
+        chute = input("Informe uma letra: ").lower().strip()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    lista_palavra_secreta[index] = letra.upper()                    
+                index += 1
+            if '_' in lista_palavra_secreta:
+                letras_restantes = lista_palavra_secreta.count('_')
+                print(f"Ainda falta descobrir {letras_restantes} letras")
+                continue
+            else:
+                print(" ")
+                print(f"Palavra secreta : {lista_palavra_secreta}")
+                print(f"Parabéns! Você acertou a palavra secreta '{palavra_secreta}'")
+                acertou = True
+                break
+        else:
+            erros += 1
+            if erros == 6:
+                enforcou = True
+                print("Você se enforcou... Tente novamente")
+            else:
+                print(f"Ops, você errou! Faltam {6 - erros} tentativas.")
 
     print(" ")
     print("{:*^60}".format("*"))
